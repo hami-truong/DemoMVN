d47e093b72fcf1c78b7cd608c2039307c2785549
admin <admin@DESKTOP-5CEKK6G>
2020-12-19 14:15:38 +0700

    add setting, class, project files to update

---
+++ src/main/java/demo/DemoMVN/App.java
@@ -0,0 +1,13 @@
+package demo.DemoMVN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
